3d47fbb10e046e44c0b9cbdd9e4076852226d162
zrmin <3547879468@qq.com>
2022-04-19 22:24:14 +0800

    templates setup

--- tyuchat/settings.py
+++ tyuchat/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,9 @@ ALLOWED_HOSTS = ['www.zrmin.top']
 # Application definition
 
 INSTALLED_APPS = [
+    # 我的apps
+    'personal',
+    # 第三方apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'tyuchat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- tyuchat/urls.py
+++ tyuchat/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from personal.views import (
+        home_screen_view
+)
 
 urlpatterns = [
+    path('', home_screen_view, name='home'),
     path('admin/', admin.site.urls),
 ]
