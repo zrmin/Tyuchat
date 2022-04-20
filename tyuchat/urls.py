58fba2f7daa6a67ed1be9affa461973c1b6ffd6a
zrmin <3547879468@qq.com>
2022-04-20 12:04:44 +0800

    Header and Footer Bootstrap Style done

--- tyuchat/urls.py
+++ tyuchat/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from personal.views import (
         home_screen_view
 )
@@ -23,3 +26,8 @@ urlpatterns = [
     path('', home_screen_view, name='home'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
