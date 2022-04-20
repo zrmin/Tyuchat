31e6f151ef17535ef7136b13914d401384aa641f
zrmin <3547879468@qq.com>
2022-04-21 00:56:27 +0800

    Register New Users and Login and Logout done

--- tyuchat/urls.py
+++ tyuchat/urls.py
@@ -21,9 +21,19 @@ from personal.views import (
         home_screen_view
 )
 
+from account.views import (
+        register_view,
+        login_view,
+        logout_view,
+)
+
 urlpatterns = [
     path('', home_screen_view, name='home'),
     path('admin/', admin.site.urls),
+
+    path('register/', register_view, name="register"),
+    path('login/', login_view, name="login"),
+    path('logout/', logout_view, name="logout"),
 ]
 
 if settings.DEBUG:
