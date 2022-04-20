9ecf883cd76aa92cf209e66f546a151c87fa50f9
zrmin <3547879468@qq.com>
2022-04-21 02:23:27 +0800

    User Account Screen

--- tyuchat/urls.py
+++ tyuchat/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
+from django.conf.urls import include
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
@@ -31,6 +32,7 @@ from account.views import (
 
 urlpatterns = [
     path('', home_screen_view, name='home'),
+    path('account/', include('account.urls', namespace='account')),
     path('admin/', admin.site.urls),
 
     path('register/', register_view, name="register"),
