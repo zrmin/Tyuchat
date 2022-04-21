f8e38ec1d9cfc9f7cd4476a809011bf08bf67e8c
zrmin <3547879468@qq.com>
2022-04-21 12:13:42 +0800

    Editing User Account Properties done

--- tyuchat/settings.py
+++ tyuchat/settings.py
@@ -142,6 +142,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760 # 10mb = 10 * 1024 *1024
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
